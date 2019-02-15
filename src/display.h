09412564f871373d76d608e708e4be9e8e8821a5
Tyler Baker <tyler@foundries.io>
2019-02-15 15:20:10 -0800

    reel_board: add basic display and temp support
    Signed-off-by: Tyler Baker <tyler@foundries.io>

---
+++ src/display.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018 Foundries.io
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef FOTA_DISPLAY_H__
+#define FOTA_DISPLAY_H__
+
+int init_display(void);
+void board_refresh_display(void);
+
+#endif	/* FOTA_DISPLAY_H__ */
--- src/main.c
+++ src/main.c
@@ -17,6 +17,11 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 #include <net/lwm2m.h>
 #include <tc_util.h>
 
+/* Display */
+#ifdef CONFIG_LWM2M_IPSO_DISPLAY
+#include "display.h"
+#endif
+
 /* Local helpers and functions */
 #include "app_work_queue.h"
 #include "product_id.h"
@@ -24,10 +29,15 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 #include "light_control.h"
 
 /* Defines and configs for the IPSO elements */
-#define TEMP_DEV		"fota-temp"
+#ifdef CONFIG_HDC1008
+#define TEMP_DEV		"HDC1010"
+#define TEMP_CHAN		SENSOR_CHAN_AMBIENT_TEMP
+#else
+#define TEMP_DEV                "fota-temp"
 #define TEMP_CHAN		SENSOR_CHAN_DIE_TEMP
+#endif
 
-static struct device *die_dev;
+static struct device *temp_dev;
 static struct float32_value temp_float;
 
 static int read_temperature(struct device *temp_dev,
@@ -70,7 +80,7 @@ static void *temp_read_cb(u16_t obj_inst_id, size_t *data_len)
 	 * This is because there is currently no way to report read_cb
 	 * failures to the LWM2M engine.
 	 */
-	read_temperature(die_dev, &temp_float);
+	read_temperature(temp_dev, &temp_float);
 	lwm2m_engine_set_float32("3303/0/5700", &temp_float);
 	*data_len = sizeof(temp_float);
 
@@ -79,11 +89,11 @@ static void *temp_read_cb(u16_t obj_inst_id, size_t *data_len)
 
 static int init_temp_device(void)
 {
-	die_dev = device_get_binding(TEMP_DEV);
-	LOG_INF("%s on-die temperature sensor %s",
-		die_dev ? "Found" : "Did not find", TEMP_DEV);
+	temp_dev = device_get_binding(TEMP_DEV);
+	LOG_INF("%s temperature sensor %s",
+		temp_dev ? "Found" : "Did not find", TEMP_DEV);
 
-	if (!die_dev) {
+	if (!temp_dev) {
 		LOG_ERR("No temperature device found.");
 		return -ENODEV;
 	}
@@ -120,6 +130,17 @@ void main(void)
 	}
 	_TC_END_RESULT(TC_PASS, "init_light_control");
 	TC_END_REPORT(TC_PASS);
+
+#ifdef CONFIG_LWM2M_IPSO_DISPLAY
+	TC_PRINT("Initializing IPSO Display\n");
+	if (init_display()) {
+		_TC_END_RESULT(TC_FAIL, "init_display");
+		TC_END_REPORT(TC_FAIL);
+		return;
+	}
+	_TC_END_RESULT(TC_PASS, "init_display");
+	TC_END_REPORT(TC_PASS);
+#endif
 
 	if (lwm2m_init(app_work_q)) {
 		return;
